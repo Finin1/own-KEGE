db86d77db5f3067a41a888b7838f56142053f47f
Finin <egor.fa_a@mail.ru>
2025-03-29 20:30:10 +0300

    fixed port

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def get_number(num: int):
 
 if __name__ == "__main__":
     host = os.getenv("HOST", "localhost")
-    port = os.getenv("PORT", "8080")
+    port = int(os.getenv("PORT", 8080))
     app.run(host=host, port=port)
