c45537b2c97b89e68abc4b00c25e108d2dc0b668
BRO-Fedka <71376448+BRO-Fedka@users.noreply.github.com>
2025-03-30 03:31:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-import os
-from typing import Any
+from flask import Flask, render_template, session, redirect, request
+from typing import Any, NamedTuple,List
 from pathlib import Path
-from flask import Flask
+
+
+class Task(NamedTuple):
+    num: int
+    answer_input_type: str = 'line'
+    files: List[str] = []
+
 
 app = Flask(__name__)
+app.app_context()
+app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "0"
+current_task_nums = [Task(1),Task(2),Task(3),Task(4),Task(5),Task(6),Task(7),Task(8),Task(9),Task(10)]
 
 
-@app.get("/task/<int:num>")
+@app.route('/', methods=["GET"])
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+
+
+@app.route('/task/', methods=["GET", "POST"])
+def task():
+    if request.method == 'GET':
+        if 'code' in session:
+            return render_template('task.html', tasks=current_task_nums)
+        else:
+            return render_template('error.html', title='Много хочешь, ВВЕДИ КОД')
+    elif request.method == 'POST':
+        try:
+            request.form['code']
+        except:
+            return render_template('error.html', title='Думаешь самый умный ?')
+        if len(request.form['code']) == 0:
+            return render_template('error.html', title='Код активации нужно ВВЕСТИ')
+        session['code'] = request.form['code']
+        return render_template('task.html', tasks=current_task_nums)
+
+
+@app.route('/finish/', methods=["POST"])
+def finish():
+    if not 'code' in session:
+        return render_template('error.html', title='Ты сломал систему, МОЛОДЕЦ')
+    print(session['code'])
+    print(request.form)
+    session.pop('code')
+    return render_template('finish.html')
+
+
+@app.route("/task/<num>")
 def get_number(num: int):
-    response: dict[str, Any] = {}
-    response["number"] = num
-    path_to_template = Path("template", f"task{num}.html")
-    response["content"] = open(path_to_template, encoding="utf-8").readline()
-    return response
+    print(num)
+    return render_template(f"templates/task{num}.html")
 
 
-if __name__ == "__main__":
-    host = os.getenv("HOST", "localhost")
-    port = int(os.getenv("PORT", 8080))
-    app.run(host=host, port=port)
+if __name__ == '__main__':
+    app.run(host='localhost', port=80, debug=True)
