2f0cb9e56bd1a7322f81253a8f26f849988a7cfe
BRO_Fedka <fedya_dot@mail.ru>
2025-04-27 18:18:59 +0300

    lol-baloons+ZIP FILE !!!

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import datetime
 from random import randint
 from flask import Flask, render_template, session, request
 from typing import NamedTuple, List, Union
+from pathlib import Path
+import logging
 
 try:
     from sqlalchemy import select as Select
@@ -11,6 +13,7 @@ except Exception:
 
 from database import create_db, create_session
 from database import Student, StudentAnswer, Task as TaskModel
+import random
 
 
 class Task(NamedTuple):
@@ -26,6 +29,10 @@ app.config['SECRET_KEY'] = "0"
 current_task_nums: List[Task] = []
 
 
+def render_error(title):
+    return render_template('error.html', title=title, baloons =[[random.randint(2,98),random.randint(7,15),random.randint(0,8)/2] for _ in range(0,randint(10,15))])
+
+
 def update_task_list() -> None:
     global current_task_nums
     current_task_nums = []
@@ -37,10 +44,10 @@ def update_task_list() -> None:
             number = task.task_number
             answer = task.task_answer
             files = task.files
-            
+
             hight = answer.count("\n") + 1
             width = answer.split("\n")[0].count(' ') + 1
-            
+
             if number == 25:
                 hight += randint(1, 5)
             if hight == 1 and width == 1:
@@ -51,7 +58,7 @@ def update_task_list() -> None:
             files_list = []
             for file in files:
                 files_list.append(file.file_name)
-            
+
             current_task_nums.append(Task(number, task_type, files_list))
 
 
@@ -66,14 +73,16 @@ def task():
     term = datetime.timedelta(minutes=55, hours=3)
     print('time', term)
     # print('time',dir(term))
-    caption = "ЕГЭ инфа [<Имя ученика>]"
+    caption = "ЕГЭ информатика"
     if request.method == 'GET':
         if 'code' in session:
             return render_template('task.html', tasks=current_task_nums, term=term, caption=caption)
         else:
-            return render_template('error.html', title='Много хочешь, ВВЕДИ КОД')
+            return render_error(title='Много хочешь, ВВЕДИ КОД')
     elif request.method == 'POST':
         try:
+            if not request.form['code']:
+                return render_error( title='Много хочешь, ВВЕДИ КОД')
             with create_session() as db_session:
                 current_code = int(request.form['code'])
 
@@ -81,24 +90,31 @@ def task():
                 student = db_session.scalars(student_statement).one_or_none()
 
                 if student is None:
-                    return render_template("error.html", title="СВОЙ КОД ВВЕДИ")
+                    return render_error(title="Введи верный код")
 
                 if student.done:
-                    return render_template("error.html", title="Ты уже всё решил")
+                    return render_error(title="Ты уже всё решил :)")
         except Exception:
-            return render_template('error.html', title='Думаешь самый умный ?')
+            return render_error( title='Думаешь самый умный ?')
         if len(request.form['code']) == 0:
-            return render_template('error.html', title='Код активации нужно ВВЕСТИ')
+            return render_error( title='Код активации нужно ВВЕСТИ')
         session['code'] = request.form['code']
-        return render_template('task.html', tasks=current_task_nums, term=term, caption=caption)
+        return render_template('task.html', tasks=current_task_nums, term=term,
+                               caption=caption + f" [{session['code']}]")
 
 
 @app.route('/finish/', methods=["POST"])
 def finish():
     if 'code' not in session:
-        return render_template('error.html', title='Ты сломал систему, МОЛОДЕЦ')
+        return render_error( title='Ты сломал систему, МОЛОДЕЦ')
     code = session['code']
-
+    try:
+        print(request.files)
+        print(request.form)
+        f = request.files['solution_file']
+        f.save(Path('solutions', str(code) + '.' + f.filename.split('.')[-1]))
+    except:
+        logging.exception('')
     request_items = request.form.items()
     request_items = filter(lambda el: "answer" in el[0], request_items)
     request_items = [(el[0].replace("answer", ""), el[1]) for el in request_items]
@@ -114,7 +130,7 @@ def finish():
             answer = "None"
         number = int(number)
         all_answers[number] = answer.strip().lower()
-    
+
     table_answers = {}
     for answer_info, answer in table_items:
         number, column, row = map(int, answer_info)
@@ -148,12 +164,12 @@ def finish():
                 task_statement = Select(TaskModel).where(TaskModel.task_number == number)
                 task = db_session.scalars(task_statement).one_or_none()
                 if task is None:
-                    return render_template("error.html", title="Чё-то многова-то номеров")
+                    return render_error(title="Чё-то многова-то номеров")
 
                 new_student_answer = StudentAnswer(student_id=student.id,
                                                    task_id=task.id,
                                                    student_answer=answer)
-                
+
                 db_session.add(new_student_answer)
                 student.done = False  # ! CHANGE IN THE END
                 db_session.merge(student)
